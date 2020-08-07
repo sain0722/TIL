c85b7f4b841717b4124c1738711442f5388bb003
sain0722 <sain0722@naver.com>
2020-08-07 09:52:09 +0900

    My_Main

--- pytorch/covid_classification/main.py
+++ pytorch/covid_classification/main.py
@@ -11,7 +11,9 @@ import argparse
 from dataloader import data_loader
 from evaluation import evaluation_metrics
 from model import Vgg19
+from torch.nn.parallel.data_parallel import DataParallel
 from tensorboardX import SummaryWriter #Modify
+
 
 DATASET_PATH = os.path.join('./data')
 
@@ -44,7 +46,7 @@ def feed_infer(output_file, infer_func):
     predictions_str = []
     for index, name in enumerate(prediction_name):
         test_str = name + ' ' + str(prediction_class[index])
-        predictions_str.append(test_str) 
+        predictions_str.append(test_str)
     with open(output_file, 'w') as file_writer:
         file_writer.write("\n".join(predictions_str))
 
@@ -72,13 +74,18 @@ def save_model(model_name, model, optimizer, scheduler):
         'optimizer': optimizer.state_dict(),
         'scheduler': scheduler.state_dict()
     }
-    torch.save(state, os.path.join(model_name + '.pth'))
+    # ========================= 수정 =======================
+    # torch.save(state, os.path.join(model_name + '.pth'))
+
+    torch.save(state, os.path.join('./saved_model/' + model_name + '.pth'))
     print('model saved')
 
 
 def load_model(model_name, model, optimizer=None, scheduler=None):
     state = torch.load(os.path.join(model_name))
+    print("STATE COMPLETE")
     model.load_state_dict(state['model'])
+    print("load_state_dict COMPLETE")
     if optimizer is not None:
         optimizer.load_state_dict(state['optimizer'])
     if scheduler is not None:
@@ -86,14 +93,26 @@ def load_model(model_name, model, optimizer=None, scheduler=None):
     print('model loaded')
 
 
+def printModified(comment, base_lr, gamma, batch, weight_decay):
+    # print modified hyper parameter
+    print("==== modified parameter ====")
+    print("[[[{}]]]".format(comment))
+    print("lr = ", base_lr)
+    print("gamma = ", gamma)
+    print("batch = ", batch)
+    print("weight_decay = ", weight_decay)
+    print("============================")
+
+
 if __name__ == '__main__':
 
     # set params
     lr = 0.00001
-    epochs = 50
+    epochs = 500
     bt = 8
     gamma = float(0.8)
     weight_decay = float(1e-4)
+    comment = "BASE_MODEL"
 
     # mode argument
     args = argparse.ArgumentParser()
@@ -102,13 +121,12 @@ if __name__ == '__main__':
     args.add_argument("--cuda", type=bool, default=True)
     args.add_argument("--num_epochs", type=int, default=epochs)
     args.add_argument("--print_iter", type=int, default=10)
-    args.add_argument("--model_name", type=str, default="3.pth")
+    args.add_argument("--model_name", type=str, default="./saved_model/166.pth")
     args.add_argument("--prediction_file", type=str, default="prediction.txt")
     args.add_argument("--batch", type=int, default=bt)
     args.add_argument("--mode", type=str, default="train")
 
     init = 0
-
 
     config = args.parse_args()
 
@@ -119,42 +137,60 @@ if __name__ == '__main__':
     print_iter = config.print_iter
     model_name = config.model_name
     prediction_file = config.prediction_file
-    training_prediction= 'prediction2.txt'
+    training_prediction = 'prediction2.txt'
 
     batch = config.batch
     mode = config.mode
 
     # create model
     model = Vgg19(num_classes=num_classes)
+    # model = DataParallel(model)
 
-    summary = SummaryWriter()
+    # 폴더 생성 코드
+    # today = str(datetime.date.today())
+    #
+    # os.chdir('runs')
+    #
+    # path = os.getcwd()
+    #
+    # path += "/" + today
+    # if not os.path.isdir(path):
+    #     print(path, "폴더 생성")
+    #     os.mkdir(path)
+    # os.chdir('../')
 
 
     if mode == 'test':
         load_model(model_name, model)
-    if init != 0 :
+        prediction_file = "prediction_" + comment + ".txt"
+    if init != 0:
         modelName = '{}.pth'.format(init)
         print(modelName)
-        load_model(modelName, model)
+        load_model('saved_model/' + modelName, model)
 
     if cuda:
         model = model.cuda()
 
     if mode == 'train':
+        start_time = datetime.datetime.now()
+
+        # 텐서보드에 하이퍼파라미터 코멘트
+        summary = SummaryWriter(comment=comment)
+
         # define loss function
         loss_fn = nn.CrossEntropyLoss()
         if cuda:
             loss_fn = loss_fn.cuda()
 
         # set optimizer
-        optimizer = Adam([param for param in model.parameters() if param.requires_grad],lr=base_lr, weight_decay=1e-4)
+        optimizer = Adam([param for param in model.parameters() if param.requires_grad],lr=base_lr, weight_decay=weight_decay)
         # learning decay
-        scheduler = StepLR(optimizer, step_size=40, gamma=0.8)
+        scheduler = StepLR(optimizer, step_size=40, gamma=gamma)
 
         # get data loader
         train_dataloader, train_label_file = data_loader(root=DATASET_PATH, phase='train', batch_size=batch) # Modifiy
         validate_dataloader, validate_label_file = data_loader(root=DATASET_PATH, phase='validate', batch_size=batch)
-        test_dataloader, test_label_file = data_loader(root=DATASET_PATH, phase='test', batch_size=batch)# Modifiy
+        test_dataloader, test_label_file = data_loader(root=DATASET_PATH, phase='test', batch_size=batch) # Modifiy
 
         time_ = datetime.datetime.now()
         num_batches = len(train_dataloader)
@@ -162,26 +198,26 @@ if __name__ == '__main__':
 
         print("------------------------------------------------------------")
         total_params = sum(p.numel() for p in model.parameters())
-        print("num of parameter : ", total_params)
+        print("num of parameter : ",total_params)
         trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-        print("num of trainable_ parameter :", trainable_params)
+        print("num of trainable_ parameter :",trainable_params)
         print("------------------------------------------------------------")
 
-
-        # summary.add_scalar('ACC/train', 0, 0)  # Modify
-        # summary.add_scalar('ACC/validate', 0, 0)  # Modify
-        # summary.add_scalar('ACC/test', 0, 0)  # Modify
+        summary.add_scalar('ACC/train', 0, 0)  # Modify
+        summary.add_scalar('ACC/validate', 0, 0)  # Modify
+        summary.add_scalar('ACC/test', 0, 0)  # Modify
 
         for epoch in range(num_epochs):
+
+            time_ = datetime.datetime.now()
             model.train()
             name = init + epoch
             for iter_, data in enumerate(train_dataloader):
                 # fetch train data
-                _, image, is_label = data 
+                _, image, is_label = data
                 if cuda:
                     image = image.cuda()
-                    is_label = is_label.cuda() 
-
+                    is_label = is_label.cuda()
 
                 # update weight
                 pred = model(image)
@@ -201,30 +237,33 @@ if __name__ == '__main__':
             # scheduler update
             scheduler.step()
 
-            # save model 
+            # save model
             # if epoch % 500 == 0 :
 
+            save_model(str(name + 1), model, optimizer, scheduler)
+
             # train
-            trainingAcc = validate(training_prediction, model, train_dataloader, train_label_file, cuda,'Train')  # Modify
+            trainingAcc = validate(training_prediction, model, train_dataloader, train_label_file, cuda, 'Train')  # Modify
             # validate
-            valAcc = validate(prediction_file, model, validate_dataloader, validate_label_file, cuda,'Val') #Modify
+            valAcc = validate(prediction_file, model, validate_dataloader, validate_label_file, cuda, 'Val') #Modify
             # Test
             testAcc = validate(prediction_file, model, test_dataloader, test_label_file, cuda, 'Test')  # Modify
 
-            if testAcc < 0.96:
-                save_model(str(name + 1), model, optimizer, scheduler)
-
-
             summary.add_scalar('ACC/validate', valAcc, name) #Modify
-
-            summary.add_scalar('ACC/train', trainingAcc, name)  # Modify
 
             summary.add_scalar('ACC/test', testAcc, name) #Modify
 
+            summary.add_scalar('ACC/train', trainingAcc, name)  # Modify
 
-            time_ = datetime.datetime.now()
             elapsed = datetime.datetime.now() - time_
+
+            # print modified hyper parameter
+            printModified(comment, base_lr, gamma, batch, weight_decay)
+
             print('[epoch {}] elapsed: {}'.format(name + 1, elapsed)) #Modify
+
+        end_time = datetime.datetime.now()
+        print("Total Elapsed Time: {}".format(end_time - start_time))
 
     elif mode == 'test':
         model.eval()
